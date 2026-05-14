7ce4c233ecb551e8bc60c094ca9ed7fed751a774
Fritz-Finance-App <fritzfinance@outlook.com>
2026-05-13 23:25:50 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
  #!/bin/sh
-  echo "=== DATABASE_URL: $(echo $DATABASE_URL | cut -c1-50)..."
-  echo "=== Prisma dir:"
+  echo "=== Custom startup ==="
+  echo "DATABASE_URL: $(echo $DATABASE_URL | cut -c1-60)..."
+  echo "=== Prisma dir ==="
   ls /app/prisma/
   echo "=== Copiando migrations ==="
-  cp -r /app/prisma/postgresql-migrations /app/prisma/migrations || echo "ERRO no cp"
+  cp -r /app/prisma/postgresql-migrations /app/prisma/migrations 2>&1 || echo "ERRO no cp"
   echo "=== Rodando migrations ==="
-  cd /app && DATABASE_URL="$DATABASE_URL" ./node_modules/.bin/prisma migrate deploy --schema /app/prisma/postgresql-schema.prisma
+  cd /app && DATABASE_URL="$DATABASE_URL" ./node_modules/.bin/prisma migrate deploy --schema /app/prisma/postgresql-schema.prisma 2>&1
   echo "=== Iniciando app ==="
   exec node /app/dist/main.js
